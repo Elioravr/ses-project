557cad3f3eaef7cc702e10da4b6f8a06038dfb6a
Elior Avramoviz <elioravr@gmail.com>
2016-12-22 00:07:24 +0200

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 
 import Welcome from './components/Welcome';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Welcome />
+      <div>
+        <div className="background"></div>
+        <div className="App">
+          <Welcome />
+        </div>
       </div>
     );
   }
